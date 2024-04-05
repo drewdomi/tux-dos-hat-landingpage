7fe6cb9b228a545e530317a7cc118adce542d905
Andrew Domingues <drewdomi@proton.me>
2024-04-04 21:23:37 -0300

    feat: create links & sidebar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,42 +6,72 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
-import { MenuIcon } from "lucide-react";
-import { Button } from "../components/ui/button"
-
+import "@/styles/Header.css";
+import { SiDiscord } from "react-icons/si";
+import { LuInfo, LuUserCircle2, LuMenu } from "react-icons/lu";
 
 function Header() {
   return (
-    <header className="flex justify-between items-center p-4 px-5 max-w-screen-lg mx-auto">
+    <header className="flex justify-between items-center py-6 px-4 max-w-screen-lg mx-auto">
       <div>
-        <img src="/_76c8f30d-930b-40e7-8444-369a12e85e7d.jpg" alt="" width="70px" height="70px" className="[border-radius:50%;]"/>
+        <img
+          src="/_76c8f30d-930b-40e7-8444-369a12e85e7d.jpg"
+          alt=""
+          width="70px"
+          height="70px"
+          className="[border-radius:50%;]"
+        />
       </div>
 
       <ul className="gap-6 hidden md:flex">
         <li>
-          <a href="#">img</a>
+          <a href="https://discord.gg/pqZ64SGaVG">
+            <SiDiscord />
+            Discord
+          </a>
         </li>
         <li>
-          <a href="#">Sobre</a>
+          <a href="#sobre">
+            <LuInfo />
+            Sobre
+          </a>
         </li>
         <li>
-          <a href="#">Contato</a>
+          <a href="#contato">
+            <LuUserCircle2 />
+            Contato
+          </a>
         </li>
       </ul>
 
       <div className="block md:hidden">
         <Sheet>
-          <SheetTrigger>
-            <MenuIcon className="w-8 h-8" />
+          <SheetTrigger className="pr-3">
+            <LuMenu className="w-8 h-8" />
           </SheetTrigger>
-          <SheetContent side="left">
-            <SheetHeader>
-              <SheetTitle>Are you absolutely sure?</SheetTitle>
-              <SheetDescription>
-                This action cannot be undone. This will permanently delete your
-                account and remove your data from our servers.
-              </SheetDescription>
-            </SheetHeader>
+          <SheetContent side="left" className="pt-32">
+            <SheetDescription>
+              <ul className="ml-4 gap-5 text-2xl flex flex-col">
+                <li>
+                  <a href="https://discord.gg/pqZ64SGaVG">
+                    <SiDiscord />
+                    Discord
+                  </a>
+                </li>
+                <li>
+                  <a href="#sobre">
+                    <LuInfo />
+                    Sobre
+                  </a>
+                </li>
+                <li>
+                  <a href="#contato">
+                    <LuUserCircle2 />
+                    Contato
+                  </a>
+                </li>
+              </ul>
+            </SheetDescription>
           </SheetContent>
         </Sheet>
       </div>
@@ -50,15 +80,3 @@ function Header() {
 }
 
 export default Header;
-
-{
-  /* <style>
-  svg {
-    @apply inline
-      w-8
-      h-8
-      text-8xl
-      cursor-pointer;
-  }
-</style> */
-}
